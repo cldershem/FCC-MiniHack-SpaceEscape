e0c2c31b845909839b2fc3d9bc5b909ea8206bfb
Cameron Dershem <cldershem@gmail.com>
2017-02-26 16:57:49 -0500

    c&p of gwen's codepen

--- app.js
+++ app.js
@@ -1,28 +1,59 @@
-function loadStory() {
-  var fakeStory = [
-    'line 1',
-    'line 2',
-    'line 3',
-    'line 4',
-    'line 5',
-    'line 6',
-    'line 7',
-    'line 8',
-  ]
-  for (var i=0; i<fakeStory.length; i++) {
-    $('#story').append('<li>' + fakeStory[0] + '</li>');
-  }
+var gameState = {
+	location: "some room",
+	resources: {
+		"wood": {
+      text: "gethered some wood",
+			amount: 0,
+			display: true
+		},
+		"metal": {
+      text: "gethered some metal",
+      amount: 0,
+			display: true
+		}
+  },
+	buttons: [
+		{
+			name: "Gather Wood",
+			display: true,
+			action: 'updateWood'
+		},
+    {
+			name: "Gather Metal",
+			display: true,
+			action: 'updateMetal'
+		}
+	]
 }
 
-function loadButtons() {
+localStorage.setItem('wood', 0)
+localStorage.setItem('metal', 0)
+
+
+gameState.buttons.map(function (button) {
+	if (button.display) {
+		$('.main-area').append('<button class="'+button.action+'"">'+button.name+'</button>')
+	}
+})
+
+$('.updateWood').on('click', function (e) {
+	var temp = parseInt(localStorage.getItem('wood')) + 1;
+  $('.main-feed').prepend('<p>'+gameState.resources.wood.text+'</p>')
+	localStorage.setItem('wood', temp)
+  $('.wood').text(temp)
+  checker()
+})
+
+$('.updateMetal').on('click', function (e) {
+	var temp = parseInt(localStorage.getItem('metal')) + 1;
+  $('.main-feed').prepend('<p>'+gameState.resources.metal.text+'</p>')
+	localStorage.setItem('metal', temp)
+  $('.metal').text(temp)
+})
+
+
+function checker () {
+  
 }
 
-function loadResources() {
-}
 
-function displayModal(message) {
-}
-
-$(document).ready(function() {
-  loadStory();
-});
