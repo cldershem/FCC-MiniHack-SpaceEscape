8e5f23e9635bcc8f67091f6f9681dfe463489fcd
Cameron Dershem <cldershem@gmail.com>
2017-02-26 16:37:48 -0500

    Basic structure.

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+function loadStory() {
+  var fakeStory = [
+    'line 1',
+    'line 2',
+    'line 3',
+    'line 4',
+    'line 5',
+    'line 6',
+    'line 7',
+    'line 8',
+  ]
+  for (var i=0; i<fakeStory.length; i++) {
+    $('#story').append('<li>' + fakeStory[0] + '</li>');
+  }
+}
+
+function loadButtons() {
+}
+
+function loadResources() {
+}
+
+function displayModal(message) {
+}
+
 $(document).ready(function() {
-  console.log('hello');
+  loadStory();
 });
---
+++ story.js
@@ -0,0 +1,8 @@
+var story = [
+  {
+    line: 'This is the first line',
+    btn1: ''
+
+   },
+  'This is the second line',
+]
